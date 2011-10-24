d00943f0f3c0405a8ffede21ef4eeb3fd2c0a269
shaggyalejandro <shaggyalejandro@gmail.com>
2011-10-24 13:18:47 +0100

    Close #31 Añadida la pagina de properties
    Se muestra una pagina donde el administrador puede definir las properties
    que quiere ver.

---
+++ fap/app/controllers/popups/PopupPropertiesController.java
@@ -0,0 +1,9 @@
+
+package controllers.popups;
+
+import controllers.gen.popups.PopupPropertiesControllerGen;
+			
+public class PopupPropertiesController extends PopupPropertiesControllerGen {
+
+}
+		

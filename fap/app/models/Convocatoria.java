fffe2835a4781a659dc17fce6ecfc879907e5260
Axel Hernández Ferrera <axelhzf@gmail.com>
2011-11-21 13:01:29 +0000

    Añadido estado de la convocatoria y permiso para crear nueva solicitud

---
+++ fap/app/models/Convocatoria.java
@@ -0,0 +1,50 @@
+
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.Logger;
+import play.db.jpa.JPA;
+import play.db.jpa.Model;
+import play.data.validation.*;
+import org.joda.time.DateTime;
+import models.*;
+import messages.Messages;
+import validation.*;
+import audit.Auditable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+// === IMPORT REGION START ===
+			
+// === IMPORT REGION END ===
+	
+
+@Auditable
+@Entity
+public class Convocatoria extends Singleton {
+	// Código de los atributos
+	
+	@ValueFromTable("estadoConvocatoria")
+	public String estado;
+	
+	
+	public Convocatoria (){
+		init();
+	}
+	
+
+	public void init(){
+		super.init();
+			estado = "presentacion";
+	}
+		
+	
+
+// === MANUAL REGION START ===
+			
+// === MANUAL REGION END ===
+	
+	
+	}
+		

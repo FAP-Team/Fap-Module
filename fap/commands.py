2a704a12b15a6bc00fc54c39bca26ded6b9653a9
Paco Martin <erespia2@gmail.com>
2011-11-21 11:05:09 +0000

    Property con ContextPath para problemas de rutas en Apache

--- fap/commands.py
+++ fap/commands.py
@@ -161,6 +161,7 @@ def init_application (app, args):
     FILE = open(conf, "a");
     FILE.write("\n#FAP Configuration\n");
     FILE.write("fap.app.name=" + app.name() + "\n");
+    FILE.write("#fap.ctxPath=\n");
     FILE.write("date.format=dd/MM/yyyy\n");
     FILE.write("fap.login.type.user=true\n");
     FILE.write("app.log.path=log4j-dev.properties\n"); 

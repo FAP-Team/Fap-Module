743f66d97ec506985afacaf6a16e7ce5e38f180e
Axel Hernández Ferrera <axelhzf@gmail.com>
2011-11-24 12:33:38 +0000

    Usando fixtures para borrar correctamente las tablas

--- fap/test/baremacion/BaremacionLoad.java
+++ fap/test/baremacion/BaremacionLoad.java
@@ -1,6 +1,7 @@
 package baremacion;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,11 +12,16 @@ import com.google.gson.reflect.TypeToken;
 
 import controllers.fap.InitController;
 
+import models.CEconomico;
+import models.Criterio;
+import models.Documento;
 import models.Evaluacion;
 import models.SolicitudGenerica;
 import models.TipoCriterio;
 import models.TipoEvaluacion;
+import play.db.Model;
 import play.mvc.Scope.Params;
+import play.test.Fixtures;
 import play.test.UnitTest;
 import play.vfs.VirtualFile;
 
@@ -24,8 +30,14 @@ public class BaremacionLoad extends UnitTest {
 	@Test
 	public void load() throws Throwable {
 		//Borra todas las evaluaciones y los tipos
-		Evaluacion.deleteAll();
-		TipoEvaluacion.deleteAll();
+		//CEconomico.deleteAll();
+		//Criterio.deleteAll();
+		List<Class<? extends Model>> modelsToDelete = new ArrayList<Class<? extends Model>>();
+		modelsToDelete.add(Evaluacion.class);
+		modelsToDelete.add(TipoEvaluacion.class);
+		Fixtures.delete(modelsToDelete);
+		
+		
 		
 		//Carga el tipo de evaluación a partir del GSON
 		TipoEvaluacion tipo = loadFromJson();
@@ -48,25 +60,39 @@ public class BaremacionLoad extends UnitTest {
 		List<TipoCriterio> tipocriterios = new Gson().fromJson(vf.contentAsString(), collectionType);
 		
 		//Crea el nuevo tipo de evaluacion
-		TipoEvaluacion tipoEvaluacion = new TipoEvaluacion();
-		tipoEvaluacion.nombre = "Evaluacion innovación";
-		tipoEvaluacion.criterios = tipocriterios;
+		TipoEvaluacion tipoEv = new TipoEvaluacion();
+		tipoEv.nombre = "Evaluacion innovación";
+		tipoEv.criterios = tipocriterios;
+		tipoEv.tiposDocumentos = new ArrayList<String>();
+		tipoEv.tiposDocumentos.add("eadmon://gobcan.es/tiposDocumentos/TDC000000000000000001/v01");
 		
-		return tipoEvaluacion;
+		return tipoEv;
 	}
 	
 	private Evaluacion createEvaluacion(TipoEvaluacion tipo){
 		Evaluacion evaluacion = new Evaluacion();
 		evaluacion.init(tipo);
+		
 		return evaluacion;
 	}
 	
+	private Documento createDocumento(String tipo){
+		Documento d = new Documento();
+		d.tipo = tipo;
+		return d;
+	}
+	
 	private SolicitudGenerica getOrCreateSolicitudGenerica() throws Throwable {
-		SolicitudGenerica solicitudGenerica = SolicitudGenerica.all().first();
-		if(solicitudGenerica == null){
-			solicitudGenerica = (SolicitudGenerica)InitController.inicialize();
+		SolicitudGenerica sg = SolicitudGenerica.all().first();
+		if(sg == null){
+			sg = (SolicitudGenerica)InitController.inicialize();
 		}
-		return solicitudGenerica;
+		
+		sg.documentacion.documentos.add(createDocumento("eadmon://gobcan.es/tiposDocumentos/TDC000000000000000001/v01"));
+		sg.documentacion.documentos.add(createDocumento("eadmon://gobcan.es/tiposDocumentos/TDC000000000000000001/v01"));
+		sg.documentacion.documentos.add(createDocumento("eadmon://gobcan.es/tiposDocumentos/TDC000000000000000002/v01"));
+		
+		return sg;
 	}
 	
 }

2dc853c4e49904d31042b140d49d54b1ef0842a0
Axel Hernandez Ferrera <axelhzf@gmail.com>
2012-02-23 13:58:10 +0000

    Fixed #186 Añadido test para comprobar la inyección con anotacion en controlador

---
+++ apps/test/app/controllers/PaginaInjectController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javax.inject.Inject;
+
+import services.GestorDocumentalService;
+
+import messages.Messages;
+import models.Solicitud;
+import controllers.gen.PaginaInjectControllerGen;
+
+public class PaginaInjectController extends PaginaInjectControllerGen {
+    
+    @Inject
+    private static GestorDocumentalService gestorDocumentalService;
+    
+    public static void index(String accion, Long idSolicitud) {
+        
+        if(gestorDocumentalService == null){
+            Messages.fatal("El servicio no se inyectó correctamente");
+        }
+        
+        Solicitud solicitud = null;
+        renderTemplate("gen/PaginaInject/PaginaInject.html", accion, idSolicitud, solicitud);
+    }
+}

6e08d1460d29edb56f1be84b7158dab5d6f3eb4b
Paco Martin <erespia2@gmail.com>
2012-02-24 13:36:40 +0000

    Agregando Solicitante a la aplicacion Test

---
+++ apps/test/app/controllers/popups/PopupPersonaJuridicaTablaController.java
@@ -0,0 +1,9 @@
+
+package controllers.popups;
+
+import controllers.gen.popups.PopupPersonaJuridicaTablaControllerGen;
+			
+public class PopupPersonaJuridicaTablaController extends PopupPersonaJuridicaTablaControllerGen {
+
+}
+		

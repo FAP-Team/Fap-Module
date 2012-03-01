0f2720f29cb599769ab58ab48f87b913ac36bbad
jelcaf <jelcaf@gmail.com>
2012-03-01 13:36:43 +0000

    Se permiten ordenar los combos por clave o valor.
    + Soluciona además la incidencia de Sole de ordenar el tipo de documentos a
    aportar.
    
    Conflicts:
    
    	fap/compiler/src/es.fap.simpleled/src/es/fap/simpleled/Led.xtext

--- fap/compiler/src/es.fap.simpleled.generator/src/templates/GCombo.groovy
+++ fap/compiler/src/es.fap.simpleled.generator/src/templates/GCombo.groovy
@@ -50,6 +50,14 @@ public class GCombo {
 			
 		if(combo.anchoTitulo != null)
 			params.putStr("anchoTitulo", combo.anchoTitulo)
+		
+			
+		if (combo.ordenar != null) {
+			if (combo.ordenar.equals("ordenarPorClave"))
+				params.put "ordenarPorClave", true
+			else if (combo.ordenar.equals("ordenarPorValor"))
+				params.put "ordenarPorTexto", true
+		}
 
 		if (combo.ayuda != null) {
 			if ((combo.tipoAyuda != null) && (combo.tipoAyuda.type.equals("propover")))

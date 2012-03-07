5d165e39babcbec490c3f5a96e161730b645c302
Paco Martin <erespia2@gmail.com>
2012-03-07 14:01:34 +0000

    Fixed bug en el prepararParaSubir Documento

--- fap/app/aed/AedClient.java
+++ fap/app/aed/AedClient.java
@@ -74,6 +74,7 @@ public class AedClient {
 	
 	public static String saveDocumentoTemporal(models.Documento documento, InputStream is, String filename) throws AedExcepcion {
 		//Preparamos el documento para subir al AED
+		System.out.println("Descripcion: "+documento.descripcion);
 		documento.prepararParaSubir();
 		documento.save();
 		

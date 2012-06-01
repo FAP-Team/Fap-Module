43ac5371fcc5caa84b385970bfcfb8cf5e39c6de
Elena Curbelo <ecurbelo@gmail.com>
2012-06-01 09:58:29 +0100

    - Añadiendo fichero que faltaba y actualizado doc-properties.textile

---
+++ fap/app/models/Busqueda.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.Logger;
+import play.db.jpa.JPA;
+import play.db.jpa.Model;
+import play.data.validation.*;
+import org.joda.time.DateTime;
+import models.*;
+import messages.Messages;
+import validation.*;
+import audit.Auditable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+// === IMPORT REGION START ===
+
+// === IMPORT REGION END ===
+
+@Entity
+public class Busqueda extends Singleton {
+	// Código de los atributos
+
+	public String solicitud;
+
+	public String interesado;
+
+	@ElementCollection
+	@ValueFromTable("estadosSolicitud")
+	public Set<String> estadoSolicitud;
+
+	public Boolean mostrarTabla;
+
+	public Busqueda() {
+		init();
+	}
+
+	public void init() {
+		super.init();
+
+		if (estadoSolicitud == null)
+			estadoSolicitud = new HashSet<String>();
+		mostrarTabla = false;
+
+		postInit();
+	}
+
+	// === MANUAL REGION START ===
+
+	// === MANUAL REGION END ===
+
+}

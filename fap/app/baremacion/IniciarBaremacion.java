1e994de06adb21d0af3bd6b7f89c26759e397d1c
Paco Martin <erespia2@gmail.com>
2012-06-04 11:45:32 +0100

    Documentacion de la parte de Baremacion

--- fap/app/baremacion/IniciarBaremacion.java
+++ fap/app/baremacion/IniciarBaremacion.java
@@ -5,10 +5,28 @@ import java.util.List;
 import models.CEconomico;
 import models.Criterio;
 import models.Evaluacion;
+import models.SolicitudGenerica;
 import models.TipoEvaluacion;
 import play.db.jpa.GenericModel.JPAQuery;
 import services.BaremacionService;
 
 public class IniciarBaremacion {
 	// Clase de la que extiende el Iniciar Baremacion de cada Aplicacion independiente
+	
+	public static void iniciar(){
+		
+		TipoEvaluacion tipoEvaluacion = TipoEvaluacion.all().first();
+		
+		//Comprueba que todas las solicitudes tengan su evaluación creada
+		List<SolicitudGenerica> solicitudesSinEvaluacion = SolicitudGenerica.find("select solicitud from Solicitud solicitud " +
+				       "where not exists (select evaluacion from Evaluacion evaluacion " +
+				       "where evaluacion.tipo.id=? and evaluacion.solicitud = solicitud)", tipoEvaluacion.id).fetch();
+	
+		for(SolicitudGenerica solicitud : solicitudesSinEvaluacion){
+			Evaluacion evaluacion = new Evaluacion();
+			evaluacion.init(tipoEvaluacion);
+			evaluacion.solicitud = solicitud;
+			evaluacion.save();
+		}
+	}
 }

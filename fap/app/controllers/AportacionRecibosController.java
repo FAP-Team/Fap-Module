179c719ec2dade229ee697be6118e6f3bbc05f0e
Monika Jarysz <monika.jarysz@gmail.com>
2012-07-13 11:16:42 +0100

    Modificado en AportacionRecibosController y unos  mails.

--- fap/app/controllers/AportacionRecibosController.java
+++ fap/app/controllers/AportacionRecibosController.java
@@ -19,7 +19,7 @@ public class AportacionRecibosController extends AportacionRecibosControllerGen 
 					  "where solicitud.id=? and registradas.justificante.uri is not null",
 					  idSolicitud).fetch();
 	    TableRenderResponse<Documento> response = TableRenderResponse.sinPermisos(rows);
-	    renderJSON(response.toJSON("id", "fechaSubida", "descripcion", "urlDescarga"));
+		renderJSON(response.toJSON("fechaSubida", "tipo", "descripcionVisible", "urlDescarga", "id"));
 	}
 
 }

166bb53d7d8a3d13a47be1a193e664fada936a40
Monika Jarysz <monika.jarysz@gmail.com>
2012-08-03 13:52:51 +0100

    Fixed #273. Mejorar página Documentación.

--- fap/app/controllers/DocumentosFAPController.java
+++ fap/app/controllers/DocumentosFAPController.java
@@ -1,7 +1,28 @@
 package controllers;
 
+import messages.Messages;
+import models.Documento;
+import models.SolicitudGenerica;
 import controllers.gen.DocumentosFAPControllerGen;
 
 public class DocumentosFAPController extends DocumentosFAPControllerGen {
+	public static void index(String accion, Long idSolicitud, Long idDocumento) {
+		if (accion == null)
+			accion = getAccion();
+		if (!permiso(accion)) {
+			Messages.fatal("No tiene suficientes privilegios para acceder a esta solicitud");
+			renderTemplate("fap/Documentacion/DocumentosFAP.html");
+		}
 
+		SolicitudGenerica solicitud = DocumentosFAPController.getSolicitudGenerica(idSolicitud);
+
+		Documento documento = null;
+		if ("crear".equals(accion))
+			documento = DocumentosFAPController.getDocumento();
+		else if (!"borrado".equals(accion))
+			documento = DocumentosFAPController.getDocumento(idSolicitud, idDocumento);
+
+		log.info("Visitando página: " + "fap/Documentacion/DocumentosFAP.html");
+		renderTemplate("fap/Documentacion/DocumentosFAP.html", accion, idSolicitud, idDocumento, solicitud, documento);
+	}
 }
--- fap/app/controllers/popups/DocumentosEditarFAPController.java
+++ fap/app/controllers/popups/DocumentosEditarFAPController.java
@@ -1,7 +1,28 @@
 package controllers.popups;
 
+import messages.Messages;
+import models.Documento;
+import models.SolicitudGenerica;
 import controllers.gen.popups.DocumentosEditarFAPControllerGen;
 
 public class DocumentosEditarFAPController extends DocumentosEditarFAPControllerGen {
+	public static void index(String accion, Long idSolicitud, Long idDocumento) {
+		if (accion == null)
+			accion = getAccion();
+		if (!permiso(accion)) {
+			Messages.fatal("No tiene suficientes privilegios para acceder a esta solicitud");
+			renderTemplate("fap/Documentacion/DocumentosEditarFAP.html");
+		}
 
+		SolicitudGenerica solicitud = DocumentosEditarFAPController.getSolicitudGenerica(idSolicitud);
+
+		Documento documento = null;
+		if ("crear".equals(accion))
+			documento = DocumentosEditarFAPController.getDocumento();
+		else if (!"borrado".equals(accion))
+			documento = DocumentosEditarFAPController.getDocumento(idSolicitud, idDocumento);
+
+		log.info("Visitando página: " + "fap/Documentacion/DocumentosEditarFAP.html");
+		renderTemplate("fap/Documentacion/DocumentosEditarFAP.html", accion, idSolicitud, idDocumento, solicitud, documento);
+	}
 }

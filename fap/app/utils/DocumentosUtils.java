6ee3d8ec05d7a811b0fb2da9b5cc388bcc13baa6
Paco Martin <erespia2@gmail.com>
2012-08-10 10:27:22 +0100

    Agregando nuevas funciones al modelo Documento

--- fap/app/utils/DocumentosUtils.java
+++ fap/app/utils/DocumentosUtils.java
@@ -23,4 +23,131 @@ import verificacion.ObligatoriedadDocumentosFap;
  		}
  		return false;
  	}
+	
+	/**
+	 * Indica si el tipo de documento es obligatorio.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esTipoObligatorio (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.obligatoriedad=?", tipoUri, "OBLIGATORIO").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Indica si el tipo de documento es imprescindible.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esTipoImprescindible (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.obligatoriedad=?", tipoUri, "IMPRESCINDIBLE").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Indica si el tipo de documento es condicionado manual.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esTipoCondicionadoManual (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.obligatoriedad=?", tipoUri, "CONDICIONADO_MANUAL").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Indica si el tipo de documento es condicionado automatico.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esTipoCondicionadoAutomatico (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.obligatoriedad=?", tipoUri, "CONDICIONADO_AUTOMATICO").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Devuelve el tipo de obligatoriedad del tipo de documento
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public String getTipoObligatoriedad (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=?", tipoUri).first();
+		if (td != null) {
+			return td.obligatoriedad;
+ 		}
+ 		return null;
+ 	}
+	
+	/**
+	 * Indica si el tipo de documento es aportado por el ciudadano.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esAportadoCiudadano (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.aportadoPor=?", tipoUri, "CIUDADANO").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Indica si el tipo de documento es aportado por el organismo.
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public boolean esAportadoOrganismo (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=? and td.aportadoPor=?", tipoUri, "ORGANISMO").first();
+		if (td != null) {
+			return true;
+ 		}
+ 		return false;
+ 	}
+	
+	/**
+	 * Indica por quien debe ser aportado el tipo de documento
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public String getAportadoPor (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=?", tipoUri).first();
+		if (td != null) {
+			return td.aportadoPor;
+ 		}
+ 		return null;
+ 	}
+	
+	/**
+	 * Indica cual es el tramite al que pertenece el tipo de documento
+	 * 
+	 * @param tipoUri
+	 * @return
+	 */
+	static public Tramite getTramitePertenece (String tipoUri) {
+		TipoDocumento td = TipoDocumento.find("select td from TipoDocumento td where td.uri=?", tipoUri).first();
+		if (td != null) {
+			if (td.uri != null)
+				return Tramite.find("select t from Tramite t where t.uri=?", td.uri).first();
+ 		}
+ 		return null;
+ 	}
 }

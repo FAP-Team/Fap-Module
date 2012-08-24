acf253a7c2bac4440fbeeda1bd5e647d0c649005
jelcaf <jelcaf@gmail.com>
2012-08-24 11:45:30 +0100

    + Finalmente arreglado lo de los double y moneda!!!! #281

--- fap/app/binders/DoubleBinder.java
+++ fap/app/binders/DoubleBinder.java
@@ -1,14 +1,18 @@
 package binders;
 
 import java.lang.reflect.Type;
-import java.math.BigDecimal;
+
 import java.text.Annotation;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import play.data.binding.Global;
 import play.data.binding.TypeBinder;
 
 @Global
 public class DoubleBinder implements TypeBinder<Double> {
+	
+	static final Pattern doubleNumber = Pattern.compile("[-+]?\\d{1,3}(\\.?\\d{3})*(\\,[0-9]+)?([eE][-+]?[0-9]+)?");
 	
 	/**
 	 * Sobreescribimos el Bind de las Monedas
@@ -18,15 +22,19 @@ public class DoubleBinder implements TypeBinder<Double> {
 			java.lang.annotation.Annotation[] annotations, String value,
 			Class actualClass, Type genericType) throws Exception {
 
-		if (!value.matches(".*[eE].*")) {
+		Matcher mBest = doubleNumber.matcher(value);
+		if (mBest.matches()) {
 			value = value.replaceAll("[.]", "");
-			value = value.replaceAll(",", ".");
+			value = value.replaceAll(",", ".");			
+		} else {
+			value = value.replaceAll("[.]", ",");
 		}
-		
+				
 		boolean nullOrEmpty = value == null || value.trim().length() == 0;
         if (nullOrEmpty) {
             return actualClass.isPrimitive() ? 0d : null;
         }
 		return Double.parseDouble(value);
 	}
+	
 }

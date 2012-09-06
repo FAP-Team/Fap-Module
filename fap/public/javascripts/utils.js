893f8c59b9fb7c9bc530a06e82286c93741c465a
jelcaf <jelcaf@gmail.com>
2012-09-06 10:10:09 +0100

    + No se envían ahora los campos ocultos por grupos (No es cierto, se envían, pero con un "name" diferente; así el controlador no los recibe). Lo que permite tener 2 elementos (uno visible y otro no) tirando del mismo campo sin problema.

--- fap/public/javascripts/utils.js
+++ fap/public/javascripts/utils.js
@@ -20,3 +20,60 @@ function createAndSubmitDynamicForm(action, params, $div){
 		alert("Error"+e);
 	}
 }
+
+/**
+ * Cambia el attributo <b>name</b> de los elementos descendientes del ancestro que se especifica,
+ * para que no se realice el binding en el controlador.
+ * @param idGrupo id del elemento ancestro
+ */
+function hideNameFromGrupo (idGrupo) {
+	var $node = $('#'+idGrupo);
+	$node.contents ().each (function processNodes () {
+    	var $node = $(this);
+    	if ($node.attr("name")) {
+    		$node.attr("name", "fakeNAME"+$node.attr("name"));
+    	}
+        $(this).contents ().each (processNodes);
+	});
+}
+
+/**
+ * Establece de nuevo el attributo <b>name</b> de los elementos descendientes del ancestro que se especifica,
+ * para que se realice el binding.
+ * @param idGrupo id del elemento ancestro
+ */
+function showNameFromGrupo (idGrupo) {
+	var $node = $('#'+idGrupo);
+	$node.contents ().each (function processNodes () {
+    	var $node = $(this);
+    	if ($node.attr("name")) {
+    		var myId = $node.attr("name");
+    		myId = myId.replace("fakeNAME", "");
+    		$node.attr("name", myId);
+    	}
+        $(this).contents ().each (processNodes);
+	});
+}
+
+/**
+ * Oculta el elemento y modifica el "name" si se especifica para que se envíe en el formulario, pero no lo "reciba" el controlador
+ * @param element
+ */
+function _hide (id, sendHiddenFields) {
+	var el = Ext.get(id);
+	el.setVisibilityMode(Ext.Element.OFFSETS);
+	el.hide();
+	if (!sendHiddenFields)
+		hideNameFromGrupo(id);
+}
+
+/**
+ * Muestra el elemento y modifica el "name" si se especifica para que se envíe en el formulario, pero no lo "reciba" el controlador
+ * @param element
+ */
+function _show (id, sendHiddenFields) {
+	var el = Ext.get(id).show();
+	
+	if (!sendHiddenFields)
+		showNameFromGrupo(id);
+}

346dae63f41514d70d0bf4144462324eea5b1583
Monika Jarysz <monika.jarysz@gmail.com>
2012-09-12 13:53:17 +0100

    Revert "Añadida aplicación Cuadro de Mandos"
    This reverts commit 9048df3053f64e699461fd0ece28c6929e0e908b.

--- apps/cuadroMandos/app/controllers/CuadroMandosController.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-import messages.Messages;
-import models.CuadroMando;
-import controllers.gen.CuadroMandosControllerGen;
-
-public class CuadroMandosController extends CuadroMandosControllerGen {
-
-}

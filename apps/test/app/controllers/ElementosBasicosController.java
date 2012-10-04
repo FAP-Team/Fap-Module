41acc69f7a1d8cabf61bd4fd12c4eb30954f01bb
Paco Martin <erespia2@gmail.com>
2012-10-04 10:22:16 +0100

    ElementosBasicos con Mayuscula

--- apps/test/app/controllers/ElementosBasicosController.java
+++ apps/test/app/controllers/ElementosBasicosController.java
@@ -1,7 +1,13 @@
 package controllers;
 
+<<<<<<< HEAD
 import controllers.gen.ElementosBasicosControllerGen;
 
 public class ElementosBasicosController extends ElementosBasicosControllerGen {
+=======
+import controllers.gen.elementosBasicosControllerGen;
+
+public class ElementosBasicosController extends elementosBasicosControllerGen {
+>>>>>>> ElementosBasicos con Mayuscula
 
 }

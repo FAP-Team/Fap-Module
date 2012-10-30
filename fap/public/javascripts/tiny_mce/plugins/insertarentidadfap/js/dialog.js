f1d6eb60dcb14cf4eb8ac083483e1a0d5eacd3fd
Elena Curbelo <ecurbelo@gmail.com>
2012-10-30 14:05:13 +0000

    - Corrigiendo problema con los alias vacíos y en las listas

--- fap/public/javascripts/tiny_mce/plugins/insertarentidadfap/js/dialog.js
+++ fap/public/javascripts/tiny_mce/plugins/insertarentidadfap/js/dialog.js
@@ -6,26 +6,29 @@ var InsertarEntidadFAPDialog = {
 	    tinyMCEPopup.editor.execCommand('existeIdPlantillaURL');
 	},
     
-    insertLista : function(contenido, entidad, alias, aliasAtributo, entidadAtributo, guardarAlias) {     
+    insertLista : function(contenido, entidad, alias, aliasAtributo, entidadAtributo, guardarAlias) {  
+        var idPlantilla = tinyMCEPopup.editor.plantillaEnEditor;
         if (guardarAlias) {   
             // Guardamos los alias en base de datos (el alias de la entidad principial y el del atributo)
             // Ejemplo: #{list items:aportacion.documentos, as:"documentos25"} 
             //          El alias de la entidad principal es "aportacion"
             //          El alias de la entidad del atributo es "documentos"
             //          "documentos25" es el alias del bucle #{list}
-            var idPlantilla = tinyMCEPopup.editor.plantillaEnEditor;
             var jsonAlias = '{ "' + alias +  '" : "' + entidad + '" , "' + aliasAtributo + '" : "' + entidadAtributo + '" }';
-             $.post(tinyMCE.settings.httpPath + '/plantillasdoccontroller/insertarAliasEntidad', {'idPlantilla' : idPlantilla, 'jsonAlias' : jsonAlias}, function() { ; });
+            $.post(tinyMCE.settings.httpPath + '/plantillasdoccontroller/insertarAliasEntidad', {'idPlantilla' : idPlantilla, 'jsonAlias' : jsonAlias}, function() { ; });
+         }
+         else {
+            var jsonAlias = '{ "' + aliasAtributo + '" : "' + entidadAtributo + '" }';
+            $.post(tinyMCE.settings.httpPath + '/plantillasdoccontroller/insertarAliasEntidad', {'idPlantilla' : idPlantilla, 'jsonAlias' : jsonAlias}, function() { ; });
          }
 
 		tinyMCEPopup.editor.execCommand('mceInsertContent', false, contenido);
 		tinyMCEPopup.close();
 	},
     
-	insert : function(contenido, entidad, guardarAlias) {
+	insert : function(contenido, alias, entidad, guardarAlias) {
 	    if (guardarAlias) {             // Guardamos el alias en base de datos
-            var idPlantilla = tinyMCEPopup.editor.plantillaEnEditor;
-            var alias = contenido.split(".")[0];
+            var idPlantilla = tinyMCEPopup.editor.plantillaEnEditor;      
             var jsonAlias = "{ " + alias +  " : " + entidad + " }";
             $.post(tinyMCE.settings.httpPath + '/plantillasdoccontroller/insertarAliasEntidad', {'idPlantilla' : idPlantilla, 'jsonAlias' : jsonAlias }, function() { ; });
         }

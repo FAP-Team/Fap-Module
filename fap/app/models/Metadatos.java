41141727450367c5a1c20654300ffd57ec7936d3
Elena Curbelo <ecurbelo@gmail.com>
2012-11-08 13:22:07 +0000

    Fixed #307. Carga de metadatos de los tipos de documentos

---
+++ fap/app/models/Metadatos.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+import play.Logger;
+import play.db.jpa.JPA;
+import play.db.jpa.Model;
+import play.data.validation.*;
+import org.joda.time.DateTime;
+import models.*;
+import messages.Messages;
+import validation.*;
+import audit.Auditable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+// === IMPORT REGION START ===
+
+// === IMPORT REGION END ===
+
+@Entity
+public class Metadatos extends FapModel {
+	// Código de los atributos
+
+	public String tipoDocumento;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinTable(name = "metadatos_listametadatos")
+	public List<Metadato> listaMetadatos;
+
+	public Metadatos() {
+		init();
+	}
+
+	public void init() {
+
+		if (tipoDocumento == null)
+			tipoDocumento = "ALL";
+
+		if (listaMetadatos == null)
+			listaMetadatos = new ArrayList<Metadato>();
+
+		postInit();
+	}
+
+	// === MANUAL REGION START ===
+	
+	public static void deleteAllMetadatos() {
+		List<Metadatos> listaMetadatos = Metadatos.findAll();
+		for (Metadatos metadatos : listaMetadatos) {
+			metadatos.listaMetadatos = null;
+			metadatos.delete();
+		}
+		Metadato.deleteAll();
+	}
+	
+	// === MANUAL REGION END ===
+
+}

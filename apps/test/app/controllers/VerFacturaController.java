cdd4725237c6266a797c31ec095a9807754ae206
Monika Jarysz <monika.jarysz@gmail.com>
2013-01-11 12:24:40 +0000

    Eliminados imports innecesarios del controlador VerFactura.

--- apps/test/app/controllers/VerFacturaController.java
+++ apps/test/app/controllers/VerFacturaController.java
@@ -1,9 +1,5 @@
 package controllers;
 
-import java.util.List;
-import java.util.Map;
-
-import models.ItemsFactura;
 import controllers.gen.VerFacturaControllerGen;
 
 public class VerFacturaController extends VerFacturaControllerGen {

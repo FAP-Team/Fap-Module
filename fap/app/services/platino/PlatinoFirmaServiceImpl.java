d8ef36be29316a157bf0a242a6375d322243a9e0
jelcaf <jelcaf@gmail.com>
2013-02-04 14:36:16 +0000

    + Fixed convivencia con el cambio de Websigner platino.

--- fap/app/services/platino/PlatinoFirmaServiceImpl.java
+++ fap/app/services/platino/PlatinoFirmaServiceImpl.java
@@ -158,14 +158,10 @@ public class PlatinoFirmaServiceImpl implements services.FirmaService {
         String host;
         if("pre".equals(JS_ENTORNO.toLowerCase())){
             host = "http://www-pre.gobiernodecanarias.org/platino/servicios/sfst/js/";
+            jsclient.add(host + "CAValidas.js");
         }else{
             host = "http://www.gobiernodecanarias.org/platino/servicios/sfst/js/";
-        }
-        // Si no existe la property o es true (utilizamos webSigner 6.3)
-        if ((FapProperties.get("fap.platino.websigner63") == null) || (FapProperties.getBoolean("fap.platino.websigner63"))) {
-        	jsclient.add(host + "CAValidas.js");
-        } else {
-        	jsclient.add("/public/javascript/firma/firma-WebSignerExtra.js");
+            jsclient.add("http://http://www.gobiernodecanarias.org/platino/js/CAValidas.js");
         }
         jsclient.add(host + "WS_Full.js");
         jsclient.add(host + "sfest.utils.js");

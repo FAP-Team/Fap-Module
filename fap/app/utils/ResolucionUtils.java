cb040645923886434ac5d1b7d7fb330db9dcb3d3
Yurena Cabrera <yurena.cabcas@gmail.com>
2013-05-21 08:48:31 +0100

    + Fixed bug ordenacion por criterios (nota a null)

--- fap/app/utils/ResolucionUtils.java
+++ fap/app/utils/ResolucionUtils.java
@@ -23,18 +23,19 @@ public class ResolucionUtils {
 		  public int compare(Object o1, Object o2) {
 			  LineaResolucionFAP u1 = (LineaResolucionFAP) o1;
 			  LineaResolucionFAP u2 = (LineaResolucionFAP) o2;
-			  LineaResolucionFAP aux1 = u1;;
-			  LineaResolucionFAP aux2 = u2;
+			  int result = 0;
 			  if (u1.puntuacionBaremacion == null){
 				  return BEFORE;
 			  }
-			  if (u2.puntuacionBaremacion == null){
+			  else if (u2.puntuacionBaremacion == null){
 				  return AFTER;
-			  }
-			  
-			  int result = u2.puntuacionBaremacion.compareTo(u1.puntuacionBaremacion);
+			  } else
+				  result = u2.puntuacionBaremacion.compareTo(u1.puntuacionBaremacion);
 			  
 			  if (result == 0){ //Si son iguales, se compara por criterios.
+				  System.out.println("Caso1: "+u1.solicitud.solicitante.nombreCompleto+" nota: "+u1.puntuacionBaremacion);
+				  System.out.println("Caso2: "+u2.solicitud.solicitante.nombreCompleto+" nota: "+u2.puntuacionBaremacion);
+				  
 				  return comparacionPorCriterios(u1, u2);
 			  }
 			  return result;
@@ -48,14 +49,18 @@ public class ResolucionUtils {
 			  Evaluacion evaluacionU2 = Evaluacion.find("select evaluacion from Evaluacion evaluacion where evaluacion.estado = ? and evaluacion.solicitud = ?", "evaluada", u2.solicitud).first();
 			  BaremacionUtils.ordenarCriterios(evaluacionU1.criterios);
 			  BaremacionUtils.ordenarCriterios(evaluacionU2.criterios);
-			  for (int i = 0; i <evaluacionU1.criterios.size(); i++) {
-				  if (evaluacionU1.criterios.get(i).valor < evaluacionU2.criterios.get(i).valor){
-					  return BEFORE;
-				  }
-				  if (evaluacionU1.criterios.get(i).valor > evaluacionU2.criterios.get(i).valor){
-					  return AFTER;
-				  }
-			  }
+			  
+			  for (int i = 0; i < evaluacionU1.criterios.size(); i++) {
+				 if (!evaluacionU1.criterios.get(i).tipo.jerarquia.contains(".")){ //Si no contiene . es criterio primer orden
+					if (evaluacionU1.criterios.get(i).tipo.esIgual(evaluacionU2.criterios.get(i).tipo)){ //misma jerarquia
+						//Comprobacion de valores
+						if (evaluacionU1.criterios.get(i).valor > evaluacionU2.criterios.get(i).valor)
+							return AFTER;
+						if (evaluacionU1.criterios.get(i).valor < evaluacionU2.criterios.get(i).valor)
+							return BEFORE;
+					}
+				}
+			}
 			  return IGUALES;
 		  }
 	}

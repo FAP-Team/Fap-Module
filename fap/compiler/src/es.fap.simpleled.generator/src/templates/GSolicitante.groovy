c731c667bdcd24877beac360eed96974f0a74714
eleazar <eleazar87@gmail.com>
2013-06-17 09:33:05 +0100

    FixedBug: Correción en la generación de los atributos relaciondos con sexo y fecha de nacimiento

--- fap/compiler/src/es.fap.simpleled.generator/src/templates/GSolicitante.groovy
+++ fap/compiler/src/es.fap.simpleled.generator/src/templates/GSolicitante.groovy
@@ -111,9 +111,7 @@ public class GSolicitante extends GElement{
 		fisica.name = "${solicitante.name}Fisica";
 		fisica.campo = CampoUtils.addMore(solicitante.campo, "fisica").campo;
 		fisica.requerido = solicitante.requerido;
-		fisica.noSexo = solicitante.noSexo;
 		fisica.sexo = solicitante.sexo;
-		fisica.noNacimiento = solicitante.noNacimiento;
 		fisica.nacimiento = solicitante.nacimiento;
 		
 		Grupo grupo = LedFactory.eINSTANCE.createGrupo();
@@ -144,10 +142,8 @@ public class GSolicitante extends GElement{
 				persona.requerido = true;
 				persona.name = "representanteDelSolicitante_${solicitante.name}";
 				persona.campo = CampoUtils.addMore(solicitante.campo, "representante").campo;
-				persona.noSexo = solicitante.noSexo;
-				persona.sexo = solicitante.sexo;
-				persona.noNacimiento = solicitante.noNacimiento;
-				persona.nacimiento = solicitante.nacimiento;
+				persona.sexo = false;
+				persona.nacimiento = false;
 				grupoRepFisica.elementos.add(persona);
 				textoEmail.campo = CampoUtils.addMore(persona.campo, "email").campo;
 			}
@@ -155,10 +151,8 @@ public class GSolicitante extends GElement{
 				PersonaFisica personaFisica = LedFactory.eINSTANCE.createPersonaFisica();
 				personaFisica.requerido = true;
 				personaFisica.name = "representanteDelSolicitante_${solicitante.name}";
-				personaFisica.noSexo = solicitante.noSexo;
-				personaFisica.sexo = solicitante.sexo;
-				personaFisica.noNacimiento = solicitante.noNacimiento;
-				personaFisica.nacimiento = solicitante.nacimiento;
+				personaFisica.sexo = false;
+				personaFisica.nacimiento = false;
 				personaFisica.campo = CampoUtils.addMore(solicitante.campo, "representante.fisica").campo;
 				grupoRepFisica.elementos.add(personaFisica);
 				textoEmail.campo = CampoUtils.addMore(solicitante.campo, "representante.email").campo;

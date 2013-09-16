346ba1d499215789783d0d3c311b6b3c2565eefa
alecabdia <alecabdia@gmail.com>
2013-09-16 10:08:37 +0100

    + Primeros cambios de firma de múltiples documentos

--- fap/app/models/Documento.java
+++ fap/app/models/Documento.java
@@ -69,15 +69,27 @@ public class Documento extends FapModel {
 	@ValueFromTable("estadoNotificacion")
 	public String estadoDocumento;
 
+	public Boolean firmado;
+
+	@Transient
+	public String firmadoVisible;
+
+	public Documento() {
+		init();
+	}
+
 	public void init() {
+
+		if (clasificado == null)
+			clasificado = false;
+
+		if (firmado == null)
+			firmado = false;
 
 		postInit();
 	}
 
 	// === MANUAL REGION START ===
-	public Documento() {
-		clasificado = false;
-	}
 
 	public boolean isMultiple() {
 		return (tipo != null && DocumentosUtils.esTipoMultiple(tipo));
@@ -186,6 +198,20 @@ public class Documento extends FapModel {
 			return this.descripcion;
 		}
 		return descripcionDevolver;
+	}
+
+	public String getFirmadoVisible() {
+		GestorDocumentalService gestorDocumentalService = InjectorConfig.getInjector().getInstance(GestorDocumentalService.class);
+		try {
+			String firma = gestorDocumentalService.getDocumentoFirmaByUri(uri);
+			if (firma != null && !firma.isEmpty()) {
+				play.Logger.info("El documento " + descripcionVisible + " tiene la firma (" + firma + ")");
+				return "Sí";
+			}
+		} catch (Exception e) {
+			play.Logger.error("Error al obtener el documento " + descripcionVisible + " firmado");
+		}
+		return "No";
 	}
 
 	/*

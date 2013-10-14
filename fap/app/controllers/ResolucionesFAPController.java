70c2706adbf38ec697b8fdae898a65d6a65c5eb2
alecabdia <alecabdia@gmail.com>
2013-10-14 09:53:05 +0100

    + Cambiado título Expedientes afectados, arreglado problema de variable de sesión idResolucionFAP y eliminado wiki en datos de la resolución

--- fap/app/controllers/ResolucionesFAPController.java
+++ fap/app/controllers/ResolucionesFAPController.java
@@ -1,7 +1,32 @@
 package controllers;
 
+import java.util.Map;
+
+import resolucion.ResolucionBase;
+import messages.Messages;
+import controllers.fap.AgenteController;
 import controllers.gen.ResolucionesFAPControllerGen;
 
 public class ResolucionesFAPController extends ResolucionesFAPControllerGen {
 
+	public static void index(String accion) {
+		
+		Map<String, Long> ids = (Map<String, Long>) tags.TagMapStack.top("idParams");
+		Long idResolucionFAP = ids.get("idResolucionFAP");
+		if ((idResolucionFAP != null) && (idResolucionFAP > 0)) {
+			ids.remove("idResolucionFAP");
+			log.info("Se ha borrado la variable de sesion idResolucionFAP "+ idResolucionFAP);
+		}
+		
+		if (accion == null)
+			accion = getAccion();
+		if (!permiso(accion)) {
+			Messages.fatal("No tiene permisos suficientes para realizar esta acción");
+			renderTemplate("gen/ResolucionesFAP/ResolucionesFAP.html");
+		}
+
+		log.info("Visitando página: " + "gen/ResolucionesFAP/ResolucionesFAP.html" + ", usuario: " + AgenteController.getAgente().name + " Solicitud: " + params.get("idSolicitud"));
+		renderTemplate("gen/ResolucionesFAP/ResolucionesFAP.html", accion);
+	}
+	
 }

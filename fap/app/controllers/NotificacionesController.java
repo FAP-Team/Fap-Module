3dfbc9d943d492208a435047bfe71b4d1d27f0cd
aletepe <aletepe@gmail.com>
2013-10-31 11:35:42 +0000

    + Fixed minor bug Filtro de tabla de notificaciones para no mostrar en estado "creada"

--- fap/app/controllers/NotificacionesController.java
+++ fap/app/controllers/NotificacionesController.java
@@ -10,6 +10,7 @@ import models.Interesado;
 import models.Notificacion;
 import controllers.fap.AgenteController;
 import controllers.gen.NotificacionesControllerGen;
+import enumerado.fap.gen.EstadoNotificacionEnum;
 
 public class NotificacionesController extends NotificacionesControllerGen {
 
@@ -26,7 +27,7 @@ public class NotificacionesController extends NotificacionesControllerGen {
 			for (Notificacion notificacion: rows){
 				for (Interesado interesado: notificacion.interesados){
 					if (((interesado.persona.getNumeroId() != null) && (interesado.persona.getNumeroId().equals(agente.username))) || ((agente.rolActivo.equals("gestor") || agente.rolActivo.equals("administrador") || agente.rolActivo.equals("revisor"))) 
-							&& (notificacion.fechaPuestaADisposicion != null) && (notificacion.uri!=null)){
+							&& (notificacion.fechaPuestaADisposicion != null) && (!notificacion.estado.equals(EstadoNotificacionEnum.creada.name())) && (notificacion.uri!=null)){
 						rowsFiltered.add(notificacion);
 						break;
 					}

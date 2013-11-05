884adbe6db5651c96da13604e013c02b94b6b5a0
Yurena Cabrera <yurena.cabcas@gmail.com>
2013-11-05 12:08:20 +0000

    + Modificada la condicion de entrada al job de documentos (se ejecuta a la 1 nuevamente) + Eliminados aalguinos "chivatos" de informacion en los jobs de notificaciones

--- fap/app/jobs/ActualizarDocumentacionNotificaciones.java
+++ fap/app/jobs/ActualizarDocumentacionNotificaciones.java
@@ -8,24 +8,18 @@ import play.jobs.On;
 import properties.FapProperties;
 import utils.NotificacionUtils;
 
-//@On("0 20 9 * * ?")
-@Every("15min")
+@On("0 0 1 * * ?")
 public class ActualizarDocumentacionNotificaciones extends Job {
-		
-	static Integer tiempoRefresco = 1;
 		
 	@Transactional
     public void doJob() {
 		if (AdministracionFapJobs.all() != null) {
 			AdministracionFapJobs job = AdministracionFapJobs.all().first();
-			if (job.actualizarNotificaciones) {
-		    	if ((job.valorActualizarNotificaciones != null) && (tiempoRefresco == job.valorActualizarNotificaciones)){
-		    		tiempoRefresco=1;
-			    	if ((FapProperties.get("fap.notificacion.activa") != null) && (FapProperties.getBoolean("fap.notificacion.activa")) && (FapProperties.get("fap.notificacion.procedimiento") != null) && (!(FapProperties.get("fap.notificacion.procedimiento").trim().isEmpty()))){
-			    		play.Logger.info("Iniciando Job de Recarga de Documentos de Notificaciones");
-			    		NotificacionUtils.recargarDocumentosNotificacionesFromWS(FapProperties.get("fap.notificacion.procedimiento"));
-			    	}
-		    	}
+			if (job.actualizarNotificaciones){
+			    if ((FapProperties.get("fap.notificacion.activa") != null) && (FapProperties.getBoolean("fap.notificacion.activa")) && (FapProperties.get("fap.notificacion.procedimiento") != null) && (!(FapProperties.get("fap.notificacion.procedimiento").trim().isEmpty()))){
+			    	play.Logger.info("Iniciando Job de Recarga de Documentos de Notificaciones");
+			    	NotificacionUtils.recargarDocumentosNotificacionesFromWS(FapProperties.get("fap.notificacion.procedimiento"));
+			    }
 			}
 		}
 	}
--- fap/app/jobs/ActualizarNotificaciones.java
+++ fap/app/jobs/ActualizarNotificaciones.java
@@ -50,6 +50,7 @@ public class ActualizarNotificaciones extends Job {
 			    	if ((FapProperties.get("fap.notificacion.activa") != null) && (FapProperties.getBoolean("fap.notificacion.activa")) && (FapProperties.get("fap.notificacion.procedimiento") != null) && (!(FapProperties.get("fap.notificacion.procedimiento").trim().isEmpty()))){
 			    		play.Logger.info("Recargando Notificaciones desde el WebService");
 			    		NotificacionUtils.recargarNotificacionesFromWS(FapProperties.get("fap.notificacion.procedimiento"));
+			    		
 			    		// Código Añadido (05/07/2013)	CREACION automatica de las modificaciones	    		
 //				    	if (FapProperties.getBoolean("fap.notificacion.activarModificacion")) {
 //			    			List<SolicitudGenerica> solicitudes = SolicitudGenerica.findAll();
@@ -73,18 +74,11 @@ public class ActualizarNotificaciones extends Job {
 //			    						solicitud.estado = EstadosSolicitudEnum.modificacion.name();
 //			    						solicitud.save();
 //			    					}
-//			    				}
-//			    			}
-//			    		}
-//			    	
-			    	}	//-----------------
 			    		
-		    	} else {
-		    		tiempoRefresco++;
-		    	}
+	    			}
+	    		}
 			}
 		}
-    }
-
+	}
     
 }

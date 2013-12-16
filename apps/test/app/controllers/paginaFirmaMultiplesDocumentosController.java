33685863d2409bb27dd13f7d233d031fc06d42b5
alecabdia <alecabdia@gmail.com>
2013-12-16 14:06:55 +0000

    + Fixed minor bug y ejemplo de firma de múltiples documentos en test

---
+++ apps/test/app/controllers/paginaFirmaMultiplesDocumentosController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import controllers.gen.paginaFirmaMultiplesDocumentosControllerGen;
+
+public class paginaFirmaMultiplesDocumentosController extends paginaFirmaMultiplesDocumentosControllerGen {
+
+}

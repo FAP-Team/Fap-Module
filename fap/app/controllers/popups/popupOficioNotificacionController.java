553f4b84c91df170e034cfcb0e5bf32b55c18eb8
FernandoGzm <fernandogzmr@gmail.com>
2014-05-02 07:37:51 +0100

    Fix generación de popups en minúscula

--- fap/app/controllers/popups/popupOficioNotificacionController.java
+++
@@ -1,7 +0,0 @@
-package controllers.popups;
-
-import controllers.gen.popups.popupOficioNotificacionControllerGen;
-
-public class popupOficioNotificacionController extends popupOficioNotificacionControllerGen {
-
-}

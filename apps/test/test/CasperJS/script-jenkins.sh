154e24cd12b7262d6e36ba33bdd9225535dfe970
FernandoGzm <fernandogzmr@gmail.com>
2014-07-17 09:44:46 +0100

    Renombrados tests Selenium
    Renombrados test de selenium para que Play no los detecte como tests y así desactivarlos al ejecutar el comando play auto-test.
    
    Issue: #400

--- apps/test/test/CasperJS/script-jenkins.sh
+++ apps/test/test/CasperJS/script-jenkins.sh
@@ -3,9 +3,9 @@
 echo $FAPSDK
 cd $FAPSDK/apps/test
 /opt/play-1.2.4/play test &
-PID=$!
-echo El PID del proceso es $PID
 sleep 30
+export FAPPID=`ps -u jenkins -f | awk '$9=="-javaagent:/opt/play-1.2.4/framework/play-1.2.4.jar" {print $2}'`
+echo El PID del proceso es $PID
 casperjs test `pwd`/test/CasperJS/*.test.js --xunit="$WORKSPACE/log-casperjs.xml" --no-colors
-/opt/play-1.2.4/play stop
-kill $(($PID+2))
+kill $FAPPID
+echo Acabando la ejecución... el PID era $FAPPID

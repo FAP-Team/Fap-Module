09eb699f3999cee6fde6f717a8fda5411e5906df
Byron <rubenbyron@gmail.com>
2014-09-01 09:35:34 +0100

    Se añade waitPopup en nuevo documento, aportación y preparar para firmar
    Se añade waitPopup en la subida de un nuevo documento en Solicitud en el
    apartado de Documentos, a través del botón nuevo, en el botón de Guardar.
    
    Se añade waitPopup en el botón de Guardar al subir un documento en el
    apartado de Aportación, mediante el botón nuevo de la tabla de
    Agregar Nuevos Documentos
    
    Se añade waitPopup en el botón de Preparar para firmar en el apartado
    Presentar.
    
    Issue: FAP-430.

--- fap/app/controllers/DocumentosAportacionController.java
+++ fap/app/controllers/DocumentosAportacionController.java
@@ -1,9 +1,43 @@
 
 package controllers;
 
+import messages.Messages;
+import models.Agente;
+import models.Documento;
+import models.SolicitudGenerica;
+import controllers.fap.AgenteController;
 import controllers.gen.DocumentosAportacionControllerGen;
 			
 public class DocumentosAportacionController extends DocumentosAportacionControllerGen {
+	public static void index(String accion, Long idSolicitud, Long idDocumento) {
+		if (accion == null)
+			accion = getAccion();
+		if (!permiso(accion)) {
+			Messages.fatal("No tiene suficientes privilegios para acceder a esta solicitud");
+			renderTemplate("fap/DocumentosAportacion/DocumentosAportacion.html");
+		}
 
+		SolicitudGenerica solicitud = DocumentosAportacionController.getSolicitudGenerica(idSolicitud);
+
+		Documento documento = null;
+		if ("crear".equals(accion)) {
+			documento = DocumentosAportacionController.getDocumento();
+			if (properties.FapProperties.getBoolean("fap.entidades.guardar.antes")) {
+
+				documento.save();
+				idDocumento = documento.id;
+				solicitud.aportaciones.actual.documentos.add(documento);
+				solicitud.save();
+
+				accion = "editar";
+			}
+
+		} else if (!"borrado".equals(accion))
+			documento = DocumentosAportacionController.getDocumento(idSolicitud, idDocumento);
+
+		Agente logAgente = AgenteController.getAgente();
+		log.info("Visitando página: " + "fap/DocumentosAportacion/DocumentosAportacion.html" + " Agente: " + logAgente);
+		renderTemplate("fap/DocumentosAportacion/DocumentosAportacion.html", accion, idSolicitud, idDocumento, solicitud, documento);
+	}
 }
 		

3d2ccafff7a02ab39addce32f470d13f14a44e0a
Elena <elenadiaz89@gmail.com>
2014-09-22 08:37:46 +0100

    Resolución de problemas en firma múltiple
    Se comprueba que se pueden firmar documentos ya firmados por otras
    personas y que no se firma un documento que ya ha sido firmado por el mismo
    certificado.
    
    Se añaden cambios para permitir que se pueda seguir firmando documentos,
    aún cuando uno o varios documentos de los seleccionados ya hayan sido
    firmado anteriormente por el mismo certificado, sin que se pare el proceso.
    
    Issue: FAP-466

--- fap/app/controllers/DocumentacionFAPController.java
+++ fap/app/controllers/DocumentacionFAPController.java
@@ -1,7 +1,18 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
 import messages.Messages;
+import messages.Messages.MessageType;
+import models.Documento;
+import models.Firmantes;
 import models.SolicitudGenerica;
+import platino.FirmaUtils;
 import play.mvc.Util;
 import reports.Report;
 import controllers.gen.DocumentacionFAPControllerGen;
@@ -62,4 +73,60 @@ public class DocumentacionFAPController extends DocumentacionFAPControllerGen {
 			log.info("Acción Editar de página: " + "gen/DocumentacionFAP/DocumentacionFAP.html" + " , intentada sin éxito (Problemas de Validación)");
 		DocumentacionFAPController.formAbrirPlantillaFHRender(idSolicitud);
 	}
+	
+	@Util
+	public static String firmardocumentos(Long idDocumento, String firma) {
+
+		Documento documento = Documento.find("select documento from Documento documento where documento.id=?", idDocumento).first();
+		Map<String, Object> json = new HashMap<String, Object>();
+		List<String> errores = new ArrayList<String>();
+
+		if (documento != null) {
+			
+			Messages.clear();
+			
+			play.Logger.info("Firmando documento " + documento.uri);
+
+			Map<String, Long> ids = (Map<String, Long>) tags.TagMapStack.top("idParams");
+			Map<String, Object> vars = new HashMap<String, Object>();
+			json.put("idDocumento", idDocumento);
+			json.put("firmado", false);
+			if (secure.checkAcceso("editarFirmaDocumento", "editar", ids, vars)) {
+				if (documento.firmantes == null) {
+					documento.firmantes = new Firmantes();
+					documento.save();
+				}
+				if (documento.firmantes.todos == null || documento.firmantes.todos.size() == 0) {
+					Long idSolicitud = ids.get("idSolicitud");
+					documento.firmantes.todos = calcularFirmantesdocumentos(idSolicitud);
+					documento.firmantes.save();
+				}
+				FirmaUtils.firmarDocumento(documento, documento.firmantes.todos, firma, null);
+			} else {
+				//ERROR
+				String error = "No tiene permisos suficientes para realizar la acción";
+				Messages.error(error);
+				errores.add(error);
+			}
+
+			if (!Messages.hasErrors()) {
+				play.Logger.info("Firma de documento " + documento.uri + " con éxito");
+				json.put("firmado", true);
+				return new Gson().toJson(json);
+			}
+			String error = "Firma de documento " + documento.uri + " sin éxito";
+			play.Logger.info(error);
+			errores.add(error);
+		} else {
+			String error = "Error al obtener el documento " + idDocumento;
+			play.Logger.info(error);
+			errores.add(error);
+		}
+		for (String mensaje : Messages.messages(MessageType.ERROR)) {
+			errores.add(mensaje);
+		}
+		json.put("errores", errores);
+		return new Gson().toJson(json);
+	}
+
 }

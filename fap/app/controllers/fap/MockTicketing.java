10dacc4c6cd35876c772aa8ea424636aea7edef0
Byron <rubenbyron@gmail.com>
2014-11-19 14:09:15 +0000

    Modificado el servicio de Ticketing para FileSystem
    Se modifica la implementación de Ticketing para FileSystem y el mock del
    servicio de ticketing, para en función del ticket recibido devuelva los
    datos de los agentes en cada caso.
    
    Para el ticket 1 devuelve los datos de Luke Skywalker.
    Para el ticket 2 devuelve los datos de Darth Vader.
    
    Las properties implicadas son:
    fap.login.mock.ticketing.url
    fap.platino.terceros.url
    
    Un ejemplo de uso:
    
    http://localhost:9021/aciisi/jano/pre/iccid/login/ticketing?idTicket=2
    
    Issue_Relacionada: FAP-547

--- fap/app/controllers/fap/MockTicketing.java
+++ fap/app/controllers/fap/MockTicketing.java
@@ -8,7 +8,14 @@ public class MockTicketing extends Controller {
 
 	public static void index(String asunto, String ticketing) throws Exception {
 		if (Play.mode.isDev()) {
-			FilesystemMockResult resultObject = new FilesystemMockResult("NIF", "11111111H", "http://urimock");
+			FilesystemMockResult resultObject = null;
+			if ("1".equals(ticketing)) {
+				resultObject = new FilesystemMockResult("NIF", "11111111H", "http://urimock");
+		    } else 
+		    	if ("2".equals(ticketing)) {
+		    		resultObject = new FilesystemMockResult("NIF", "12345678Z", "http://urimock");
+		    	}
+			
 			renderJSON(resultObject);
 		}
 		forbidden();

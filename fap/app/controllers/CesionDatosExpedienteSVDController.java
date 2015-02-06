2382181345a9e6d59b4b32f6ef96d686607b57d2
Elena <elenadiaz89@gmail.com>
2015-02-06 09:42:01 +0000

    Implementados métodos de enviar solicitudes de transmisión
    Se implementan los métodos de Enviar solicitudes, tanto de Identidad como
    de Residencia, que se corresponden con los botones de la tabla "Enviar
    Solicitudes Identidad" y "Enviar Solicitudes Residencia" respectivamente.
    Estos métodos recogen las solicitudes de transmisión seleccionadas en la
    tabla, crean una petición con dichas solicitudes y se realiza la petición
    al servicio. Se recibe la respuesta y se parsea a una respuesta de FAP.

--- fap/app/controllers/CesionDatosExpedienteSVDController.java
+++ fap/app/controllers/CesionDatosExpedienteSVDController.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import messages.Messages;
 import models.Agente;
+import models.PeticionSVDFAP;
 import models.SolicitudGenerica;
 import models.SolicitudTransmisionSVDFAP;
+import services.SVDService;
+import services.SVDServiceException;
+import utils.SVDUtils;
+import config.InjectorConfig;
 import controllers.fap.AgenteController;
 import controllers.gen.CesionDatosExpedienteSVDControllerGen;
+import es.gobcan.platino.servicios.svd.Respuesta;
 
 public class CesionDatosExpedienteSVDController extends CesionDatosExpedienteSVDControllerGen {
 
@@ -62,6 +69,67 @@ public class CesionDatosExpedienteSVDController extends CesionDatosExpedienteSVD
 		tables.TableRenderResponse<SolicitudTransmisionSVDFAP> response = new tables.TableRenderResponse<SolicitudTransmisionSVDFAP>(rowsFiltered, true, true, false, "adminOrGestor", "adminOrGestor", "", getAccion(), ids);
 
 		renderJSON(response.toJSON("id", "datosGenericos.solicitante.nombreSolicitante", "datosGenericos.solicitante.unidadTramitadora", "datosGenericos.solicitante.idExpediente", "datosGenericos.solicitante.consentimiento", "fechaCreacion", "estado"));
+	}
+
+	public static void enviarSolicitudesIdentidad(Long id, List<Long> idsSeleccionados) {
+		//Sobreescribir para incorporar funcionalidad
+		//No olvide asignar los permisos
+		//index();
+
+		SVDService svdService = InjectorConfig.getInjector().getInstance(SVDService.class);
+
+		PeticionSVDFAP peticion = new PeticionSVDFAP();
+		List<SolicitudTransmisionSVDFAP> listaSolicitudesTransmision = new ArrayList<SolicitudTransmisionSVDFAP>();
+
+		for (Long idSolicitudTransmision: idsSeleccionados) {
+			SolicitudTransmisionSVDFAP solicitudTransmision = EditarSolicitudTransmisionSVDIdentidadController.getSolicitudTransmisionSVDFAP(idSolicitudTransmision);
+			listaSolicitudesTransmision.add(solicitudTransmision);
+		}
+
+		svdService.crearPeticion(peticion, listaSolicitudesTransmision);
+
+		try {
+			Respuesta respuesta = svdService.enviarPeticionSincrona(peticion);
+			if (!Messages.hasErrors()) {
+				SVDUtils.respuestaSincronaPlatinoToRespuestaFAP(respuesta, peticion);
+			}
+		} catch (SVDServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		CesionDatosExpedienteSVDController.editarRender(peticion.solicitudesTransmision.get(0).solicitud.id);
+
+	}
+
+	public static void enviarSolicitudesResidencia(Long id, List<Long> idsSeleccionados) {
+		//Sobreescribir para incorporar funcionalidad
+		//No olvide asignar los permisos
+		//index();
+
+		SVDService svdService = InjectorConfig.getInjector().getInstance(SVDService.class);
+
+		PeticionSVDFAP peticion = new PeticionSVDFAP();
+		List<SolicitudTransmisionSVDFAP> listaSolicitudesTransmision = new ArrayList<SolicitudTransmisionSVDFAP>();
+
+		for (Long idSolicitudTransmision: idsSeleccionados) {
+			SolicitudTransmisionSVDFAP solicitudTransmision = EditarSolicitudTransmisionSVDResidenciaController.getSolicitudTransmisionSVDFAP(idSolicitudTransmision);
+			listaSolicitudesTransmision.add(solicitudTransmision);
+		}
+
+		svdService.crearPeticion(peticion, listaSolicitudesTransmision);
+
+		try {
+			Respuesta respuesta = svdService.enviarPeticionSincrona(peticion);
+			if (!Messages.hasErrors()) {
+				SVDUtils.respuestaSincronaPlatinoToRespuestaFAP(respuesta, peticion);
+			}
+		} catch (SVDServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		CesionDatosExpedienteSVDController.editarRender(peticion.solicitudesTransmision.get(0).solicitud.id);
 	}
 
 }

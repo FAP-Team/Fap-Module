4fa62aea757d038fe4bbad8253629804173ef042
Elena <elenadiaz89@gmail.com>
2015-03-20 10:26:54 +0000

    Edición de las peticiones asíncronas
    Se implementa la opción de edición de las peticiones asíncronas. Se
    sobrescriben los métodos de las tablas de Solicitudes de Transmisión para
    que se muestren las que pertenecen a la petición que se está editando en
    ese momento, así como la vista correspondiente.
    
    Se pasa el parámetro idPeticionSVDFAP por parámetro para que los métodos
    de tablas puedan recibirlo y filtrar correctamente.

--- fap/app/controllers/EditarPeticionSVDFAPController.java
+++ fap/app/controllers/EditarPeticionSVDFAPController.java
@@ -3,17 +3,47 @@ package controllers;
 import java.util.List;
 import java.util.Map;
 
+import messages.Messages;
+import models.Agente;
+import models.PeticionSVDFAP;
 import models.SolicitudTransmisionSVDFAP;
+import controllers.fap.AgenteController;
 import controllers.gen.EditarPeticionSVDFAPControllerGen;
 
 public class EditarPeticionSVDFAPController extends EditarPeticionSVDFAPControllerGen {
 
+	public static void index(String accion, Long idPeticionSVDFAP) {
+		if (accion == null)
+			accion = getAccion();
+		if (!permiso(accion)) {
+			Messages.fatal("No tiene permisos suficientes para realizar esta acción");
+			renderTemplate("fap/EditarPeticionSVDFAP/EditarPeticionSVDFAP.html");
+		}
+		checkRedirigir();
 
-	//TODO: Falta filtrar tablas para que aparezcan sólo las solicitudes de transmisión
-	//de la petición correspondiente
+		PeticionSVDFAP peticionSVDFAP = null;
+		if ("crear".equals(accion)) {
+			peticionSVDFAP = EditarPeticionSVDFAPController.getPeticionSVDFAP();
+			if (properties.FapProperties.getBoolean("fap.entidades.guardar.antes")) {
+
+				peticionSVDFAP.save();
+				idPeticionSVDFAP = peticionSVDFAP.id;
+
+				accion = "editar";
+			}
+
+		} else if (!"borrado".equals(accion))
+			peticionSVDFAP = EditarPeticionSVDFAPController.getPeticionSVDFAP(idPeticionSVDFAP);
+
+		Agente logAgente = AgenteController.getAgente();
+		log.info("Visitando página: " + "fap/EditarPeticionSVDFAP/EditarPeticionSVDFAP.html" + " Agente: " + logAgente);
+		renderTemplate("fap/EditarPeticionSVDFAP/EditarPeticionSVDFAP.html", accion, idPeticionSVDFAP, peticionSVDFAP);
+	}
+
+
 	public static void tablatablaSolicitudesTransmisionIdentidad(Long idPeticion) {
 
-		java.util.List<SolicitudTransmisionSVDFAP> rows = SolicitudTransmisionSVDFAP.find("select solicitudTransmisionSVDFAP from SolicitudTransmisionSVDFAP solicitudTransmisionSVDFAP").fetch();
+		java.util.List<SolicitudTransmisionSVDFAP> rows = SolicitudTransmisionSVDFAP.find("select solicitudTransmisionSVDFAP from PeticionSVDFAP peticionSVDFAP join peticionSVDFAP.solicitudesTransmision solicitudTransmisionSVDFAP where peticionSVDFAP.id=?", idPeticion).fetch();
 
 		Map<String, Long> ids = (Map<String, Long>) tags.TagMapStack.top("idParams");
 		List<SolicitudTransmisionSVDFAP> rowsFiltered = rows; //Tabla sin permisos, no filtra
@@ -23,9 +53,10 @@ public class EditarPeticionSVDFAPController extends EditarPeticionSVDFAPControll
 		renderJSON(response.toJSON("id", "nombreServicio", "datosEspecificos.solicitud.id", "fechaCreacion", "estado", "respuesta.datosGenericos.transmision.fechaGeneracion", "descargarPDF"));
 	}
 
+
 	public static void tablatablaSolicitudesTransmisionResidencia(Long idPeticion) {
 
-		java.util.List<SolicitudTransmisionSVDFAP> rows = SolicitudTransmisionSVDFAP.find("select solicitudTransmisionSVDFAP from SolicitudTransmisionSVDFAP solicitudTransmisionSVDFAP").fetch();
+		java.util.List<SolicitudTransmisionSVDFAP> rows = SolicitudTransmisionSVDFAP.find("select solicitudTransmisionSVDFAP from PeticionSVDFAP peticionSVDFAP join peticionSVDFAP.solicitudesTransmision solicitudTransmisionSVDFAP where peticionSVDFAP.id=?", idPeticion).fetch();
 
 		Map<String, Long> ids = (Map<String, Long>) tags.TagMapStack.top("idParams");
 		List<SolicitudTransmisionSVDFAP> rowsFiltered = rows; //Tabla sin permisos, no filtra

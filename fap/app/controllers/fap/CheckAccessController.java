affa139169bd273a296d8018d6bfabafd9e76a2a
Byron <rubenbyron@gmail.com>
2015-03-27 10:57:12 +0000

    Se modifica la forma de autenticar al usuario de sesión.
    Se eliminan la implementación utilizada con threads locales y se utiliza
    la sesión para buscar el usuario en la base de datos cada vez que sea
    necesario, en el checkeo se comprueba que hash de sesión del usuario
    coincida con los datos de la sesión y en caso de no coincidir se procede a
    sacar de la aplicación al usuario forzando un logout.

--- fap/app/controllers/fap/CheckAccessController.java
+++ fap/app/controllers/fap/CheckAccessController.java
@@ -8,11 +8,11 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.MDC;
+import org.hibernate.dialect.function.NoArgSQLFunction;
 
-
+import controllers.gen.LogOutSedeControllerGen;
 import messages.Messages;
 import models.*;
-
 import platino.FirmaClient;
 import platino.InfoCert;
 import play.Play;
@@ -30,11 +30,12 @@ import ugot.recaptcha.Recaptcha;
 import ugot.recaptcha.RecaptchaCheck;
 import ugot.recaptcha.RecaptchaValidator;
 
-
 public class CheckAccessController extends Controller{
 
 	/**
-	 * Comprueba que el usuario está logueado en páginas que requieran login
+	 * Comprueba que el usuario está logueado en páginas que requieran login, en el caso de que no
+	 * exista un usuario conectado o se produzca un error en el checkeo del usuario,  
+	 * se redirije a una página fuera de la aplicación que indica un error de sesión.
 	 * @throws Throwable
 	 */
     @Before
@@ -47,6 +48,18 @@ public class CheckAccessController extends Controller{
         	}
             SecureController.loginFap();
         }
-        AgenteController.findAgente(); //Recupera el agente de base de datos
+        
+        try {
+	        Agente currentAgent = AgenteController.findAgente();
+			if (currentAgent != null && currentAgent.sessionHash.equals(Crypto.passwordHash(Session.current().getId()))) {
+				if (FapProperties.getBoolean("fap.debug.session.agente"))
+					play.Logger.info("Agente encontrado: " + currentAgent.username);
+				MDC.put("username", currentAgent.username);
+			} else {
+				throw new Exception("Error verificando agente en checkAccess, agente es null");
+			}
+        }catch(Throwable e){
+        	SecureController.logoutFap();
+        }
     }
 }

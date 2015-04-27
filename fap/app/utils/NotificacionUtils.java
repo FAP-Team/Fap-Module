c640a1a35f2f1577d6f2450d5b00898393fafd4f
elenadiaz89 <elenadiaz89@gmail.com>
2015-04-27 13:52:19 +0100

    Implementados Obtener Descripción, Tipo y Firmado en NotificacionUtils
    Para poder obtener la información de los documentos del AED, se
    implementan los métodos obtenerDescripcionDocumento, obtenerTipoDocumento
    y obtenerFirmadoDocumento en NotificacionUtils para que estén disponibles
    de los controladores que lo necesiten.

--- fap/app/utils/NotificacionUtils.java
+++ fap/app/utils/NotificacionUtils.java
@@ -212,4 +212,28 @@ public class NotificacionUtils {
 		return uri;
 	}
 	
+	public static String obtenerDescripcionDocumento(String uriDocumento) {
+		String descripcion = "";
+		try {
+			descripcion = gestorDocumentalService.getDescripcionDocumento(uriDocumento);
+		} catch (Exception e) {
+			play.Logger.error("Se ha producido un error obteniendo la descripción del documento");
+		}	
+		return descripcion;
+	}
+	
+	public static String obtenerTipoDocumento(String uriDocumento) {
+		String tipo = "";
+		try {
+			tipo = gestorDocumentalService.getTipoDocumento(uriDocumento);
+		} catch (Exception e) {
+			play.Logger.error("Se ha producido un error obteniendo el tipo del documento");
+		}	
+		return tipo;
+	}
+	
+	public static boolean obtenerFirmadoDocumentoNotificacion(String idUsuario, String uriNotificacion, DocumentoNotificacionEnumType tipoDocumento) {
+		return notificacionService.obtenerFirmadoDocumentoNotificacion(idUsuario, uriNotificacion, tipoDocumento);
+	}
+	
 }

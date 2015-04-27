476d7a9285e3ebec4ceadf7a936c308307cae753
Byron <rubenbyron@gmail.com>
2015-04-27 12:54:12 +0100

    Añadida referencias a todos los servicios y utils.
    Se añade referencia a todas las implementaciones de los servicios y utils
    en los controladores.

--- fap/compiler/src/es.fap.simpleled.generator/src/generator/utils/Controller.groovy
+++ fap/compiler/src/es.fap.simpleled.generator/src/generator/utils/Controller.groovy
@@ -179,11 +179,9 @@ import es.mityc.facturae32.Facturae;
 import com.google.gson.Gson;
 import javax.persistence.*;
 
-import services.FirmaService;
-import services.GestorDocumentalService;
-import services.GestorDocumentalServiceException;
+import services.*;
 import com.google.inject.Inject;
-import utils.PeticionModificacion;
+import utils.*;
 import controllers.fap.AgenteController;
 
 ${withSecure}

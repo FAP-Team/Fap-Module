4d016c9f15226a42b33b4e917ffb4efd7c482a26
Elena <elenadiaz89@gmail.com>
2015-05-06 13:52:01 +0100

    Añadidos getters y setters de AtributosSVDFAP

--- fap/app/models/AtributosSVDFAP.java
+++ fap/app/models/AtributosSVDFAP.java
@@ -1,18 +1,9 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
-import play.Logger;
-import play.db.jpa.JPA;
-import play.db.jpa.Model;
-import play.data.validation.*;
-import org.joda.time.DateTime;
-import models.*;
-import messages.Messages;
-import validation.*;
-import audit.Auditable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
 
 // === IMPORT REGION START ===
 
@@ -37,6 +28,7 @@ public class AtributosSVDFAP extends FapModel {
 		init();
 	}
 
+	@Override
 	public void init() {
 
 		if (estado == null)
@@ -56,6 +48,32 @@ public class AtributosSVDFAP extends FapModel {
 	public void setCodigoCertificado(String codigoCertificado) {
 		this.codigoCertificado = codigoCertificado;
 	}
+
+	public String getIdPeticion() {
+		return idPeticion;
+	}
+
+	public void setIdPeticion(String idPeticion) {
+		this.idPeticion = idPeticion;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getNumElementos() {
+		return numElementos;
+	}
+
+	public void setNumElementos(Integer numElementos) {
+		this.numElementos = numElementos;
+	}
+
+
 
 	// === MANUAL REGION END ===
 

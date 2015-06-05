9d79506af603c078dac02391581dd7f114090b26
Tamara <tamara1986@gmail.com>
2015-06-05 09:56:39 +0100

    Corregido error test 11 de Casper
    Aprovechando la función genérica creada para seleccionar un elemento de un
    combo, se ha solucionado un error en el test 11 de Casper que fallaba.
    
    issue: FAP-613

--- apps/test/test/CasperJS/11.CombosPorDefectoComprobarValores.test.js
+++ apps/test/test/CasperJS/11.CombosPorDefectoComprobarValores.test.js
@@ -4,7 +4,9 @@ var x = require('casper').selectXPath;
 var combosDefecto = function(test) {
     utiles.changeRole(casper, "Usuario");
     utiles.abrirUltimaSolicitud();
-
+   	utiles.seleccionarElemCombo ("#solicitud_comboTest_list", "b");
+   	utiles.seleccionarElemCombo ("#solicitud_comboTest_listMultiple", ["b1","d2"]);
+    
     casper.then(function() {
         casper.test.assertFieldCSS("#solicitud_comboTest_list", "b");
         casper.test.assertFieldCSS("#solicitud_comboTest_listMultiple", ["b1","d2"]);

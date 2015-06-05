a250ccaeee40870d07ad9ce46e8ac65125d18006
Tamara <tamara1986@gmail.com>
2015-06-05 10:02:31 +0100

    Solucionado error test 12 de Casper
    Aprovechando la función genérica creada para seleccionar un elemento de un
    combo, se ha solucionado un error en el test 12 de Casper que fallaba.
    
    issue: FAP-613

--- apps/test/test/CasperJS/12.CombosSobreescritosComprobarValores.test.js
+++ apps/test/test/CasperJS/12.CombosSobreescritosComprobarValores.test.js
@@ -5,6 +5,12 @@ var combosSobreescritos = function(test) {
     utiles.changeRole(casper, "Usuario");
     utiles.abrirUltimaSolicitud();
     utiles.abrirEnlace("Combos sobreescritos");
+    utiles.seleccionarElemCombo ("#lista", "c");
+    utiles.seleccionarElemCombo ("#listaLong", "3");
+    utiles.seleccionarElemCombo ("#listaMultiple", ["a","b","c"]);
+    utiles.seleccionarElemCombo ("#listaMultipleLong", ["1","3"]);
+    utiles.seleccionarElemCombo ("#wsjson", "2");
+    utiles.seleccionarElemCombo ("#wsxml", "1");
 
     casper.then(function() {
         casper.test.assertFieldCSS("#lista", "c");
